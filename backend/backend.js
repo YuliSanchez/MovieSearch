579e7f97b4c59d3df65b2242527b68a8c57462eb
yuliv <yuliv@YULI-LAP>
2025-09-10 22:47:26 -0600

    Obtener datos mediante peticiones y mostrarlos en el genero

--- backend/backend.js
+++ backend/backend.js
@@ -5,7 +5,7 @@
 import express from "express";
 import cors from "cors"; 
 import {readFileSync} from "fs";
-const movies = JSON.parse(
+const moviesdb = JSON.parse(
     readFileSync(new URL("./data/movies.json", import.meta.url), "utf-8"));
 
 const app = express();
@@ -22,7 +22,15 @@ app.get("/api/products", (req, res) => {
     console.log(res.originalUrl);
     console.log("Hola mundo");
 
-  res.json(movies);
+  res.json(moviesdb);
+});
+
+app.get("/api/gendermoviecatalog", (req, res) => {
+  console.log("Gender Movies Catalog");
+  const generosUnicos = [...new Set(moviesdb.map(p => p.genre))];
+  console.log(generosUnicos);
+
+  res.json(generosUnicos);
 });
 
 app.listen(PORT, () => {
